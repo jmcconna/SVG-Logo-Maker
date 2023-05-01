844c6b52244bfdcd173cd885a53a629cfa10e12f
James McConnaughey <jbmcconnaughey@gmail.com>
2023-05-01 09:35:59 -0400

    got the logo generator working

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const inquirer = require('inquirer'); //include inquirer package
 const fs = require("fs"); //include the file system package
-const Shapes = require("./lib/shapes");
+const {Shape, Circle, Triangle, Square} = require("./lib/shapes");
+const { listenerRemoved } = require('emittery');
+let shapeText = "";
 
 const questions = [
     {
@@ -8,10 +10,10 @@ const questions = [
       message: 'What text would you like displayed on your logo? Enter up to three characters.',
       name: 'logoText',
       validate: async (input) => {
-        if (input !== 'y' || input !== 'n') {
-           return 'Incorrect asnwer';
+        if (input && input.length <=3) {
+           return true;
         }
-        return true;
+        return false;
      }
     },
     {
@@ -19,22 +21,46 @@ const questions = [
       message: 'What color would you like for the text? Enter a color keyword or hexadecimal number.',
       name: 'textColor',
     },
-    {
-      type: 'password',
-      message: 'Re-enter password to confirm:',
-      name: 'confirm',
-    },
-    //choose a license
+    //choose a shape
     {
     type: 'checkbox',
     name: 'shape',
     message: 'Choose a shape for your logo:',   
     choices: ["Circle", "Triangle", "Square"]
     },
+    {
+        type: 'input',
+        message: 'What color would you like for the logo? Enter a color keyword or hexadecimal number.',
+        name: 'logoColor',
+    },
   ];
 
 //Create a function to write SVG file
 function writeToFile(fileName, data) {
+    console.log(data.shape);
+    let logoShape = "";
+    switch(data.shape[0]) {
+        case "Circle":
+            //call circle constructor
+            logoShape = new Circle(data.logoColor);
+            shapeText = logoShape.render();
+            console.log("making a circle");
+            break;
+
+        case "Triangle":
+            //call triangle constructor
+            logoShape = new Triangle(data.logoColor);
+            shapeText = logoShape.render();
+            break;
+
+        case "Square":
+            //call square constructor
+            logoShape = new Square(data.logoColor);
+            shapeText = logoShape.render();
+            break;
+    }
+
+    console.log("Shape text:" + shapeText);
 
     const svgText =
         `<svg version="1.1"
@@ -42,13 +68,10 @@ function writeToFile(fileName, data) {
             xmlns="http://www.w3.org/2000/svg">
 
            ${shapeText}
-           
 
-            <text x="150" y="125" font-size="60" text-anchor="middle" fill="${textColor}">${logoText}</text>
+            <text x="150" y="125" font-size="60" text-anchor="middle" fill="${data.textColor}">${data.logoText}</text>
 
         </svg>`
-
-    //call shape constructor based on answers from data
 
 
     //create new file and stuff all that SVG goodness into it
